d84fb638d463b5a9a4707317fbee03ee7794a6e6
philatm <philamooh@gmail.com>
2023-05-18 18:14:09 +0500

    Reults are shown correctly

--- js/main.js
+++ js/main.js
@@ -1,24 +1,32 @@
-let variants = {
+const variants = {
   1: 'rock',
   2: 'paper',
   3: 'scissors',
 };
 
-let round = (userChoice) => {
+let round = (playerChoice) => {
   let computerChoice = Math.floor(Math.random()*3 + 1);
   //let userChoice = +prompt("Choose your variant:\n1 - rock\n2 - paper\n3 - scissors");
-  let resBegin = "It's " + variants[userChoice] + ' vs ' + variants[computerChoice] + ".";
+  let resBegin = "It's " + variants[playerChoice] + ' vs ' + variants[computerChoice] + ".";
   let result;
-  if (userChoice == computerChoice) {
+  if (playerChoice == computerChoice) {
     result = "It's a draw";
-  } else if (userChoice == 1 && computerChoice == 3) {
+  } else if (playerChoice == 1 && computerChoice == 3) {
     result = "You're a winner!";
-  } else if(userChoice == 3 && computerChoice == 1) {
+  } else if(playerChoice == 3 && computerChoice == 1) {
     result = "You lost. Try again";
-  } else if (userChoice > computerChoice) {
+  } else if (playerChoice > computerChoice) {
     result = "You're a winner";
   } else {
     result = "You lost. Try again";
   }
-  return resBegin + " " + result;
+  let resObj = {
+    plString: variants[playerChoice],
+    compString: variants[computerChoice],
+    result: resBegin + " " + result,
+  }
+  return resObj;
 }
+
+
+
