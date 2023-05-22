920fce5acce6bd9c056f03416498bcb0ab57faf1
philatm <philamooh@gmail.com>
2023-05-22 18:13:07 +0500

    Remove result in the beggining

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const variants = {
   3: 'scissors',
 };
 
-let round = (playerChoice) => {
+const round = (playerChoice) => {
   let computerChoice = Math.floor(Math.random()*3 + 1);
   //let userChoice = +prompt("Choose your variant:\n1 - rock\n2 - paper\n3 - scissors");
   let resBegin = "It's " + variants[playerChoice] + ' vs ' + variants[computerChoice] + ".";
@@ -28,5 +28,40 @@ let round = (playerChoice) => {
   return resObj;
 }
 
+const makePlayerChoice = (playerChoice) => {
+  let divImgResult = document.querySelector(".img-result");
 
+  let plDiv = divImgResult.firstElementChild;
+  plDiv.innerHTML = "";
+  let compDiv = divImgResult.lastElementChild;
+  compDiv.innerHTML = "";
+
+  let textDiv = document.querySelector(".text-result");
+  
+  let resObj = round(playerChoice);
+  let { plString, compString, result } = resObj;
+
+  let imgPlayer = document.createElement('img');
+  imgPlayer.src = `./img/${plString}.jpeg`;
+  imgPlayer.alt = `${plString[0].toUpperCase()}${plString.slice(1)}`;
+  plDiv.insertAdjacentElement("afterbegin", imgPlayer);
+  //hack to make results transitonable
+  imgPlayer.style.opacity = 0;
+  window.getComputedStyle(imgPlayer).opacity;
+  imgPlayer.style.opacity = 1;
+
+
+  let imgComputer= document.createElement('img');
+  imgComputer.src = `./img/${compString}.jpeg`;
+  imgComputer.alt = `${compString[0].toUpperCase()}${compString.slice(1)}`;
+  compDiv.insertAdjacentElement("afterbegin", imgComputer);
+  imgComputer.style.opacity = 0;
+  window.getComputedStyle(imgPlayer).opacity;
+  imgComputer.style.opacity = 1;
+
+  let vsDiv = document.querySelector(".vs");
+  vsDiv.classList.remove("vs-hidden");
+  
+  textDiv.textContent = result;
+}
 
